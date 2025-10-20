8375ba2490984a163f81941180cf4e9de1e646ba
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-10-20 21:03:05 +0300

    fix and finish lesson program

--- main.cpp
+++ main.cpp
@@ -10,15 +10,15 @@ int main () {
     size_t rows = 0, cols = 0;
     std::cin >> rows >> cols;
     if (!std::cin) {
-	return 1;
+	    return 1;
     }
 
     int ** m = create(rows, cols);
 
     input(m, rows, cols);
     if (!std::cin) {
-	remove(m, rows);
-	return 1;
+	    remove(m, rows);
+	    return 1;
     }
 
     output(m, rows, cols);
@@ -31,16 +31,16 @@ int main () {
 
 void input(int ** m, size_t rows, size_t cols) {
     for (size_t i = 0ull; i < rows; ++i) {
-	for (size_t j = 0ull; j < rows; ++j) {
-	    std::cin >> m[i][j];
+	    for (size_t j = 0ull; j < cols; ++j) {
+	        std::cin >> m[i][j];
         }
     }
 }
 
 void output(const int * const * m, size_t rows, size_t cols) {
     for (size_t i = 0ull; i < rows; ++i) {
-	std::cout << m[i][0];
-        for (size_t j = 1ull; j < rows; ++j) {
+	    std::cout << m[i][0];
+        for (size_t j = 1ull; j < cols; ++j) {
             std::cout << " " << m[i][j];
         }
 	std::cout << "\n";
@@ -51,12 +51,12 @@ int ** create(size_t rows, size_t cols) {
     int ** result = new int*[rows];
     size_t i = 0ull;
     try {
-    	for (i < rows; ++i;) {
+    	for (i; i < rows; ++i) {
             result[i] = new int[cols];
         }
     } catch (...) {
-	remove(result, i);
-	throw;
+	    remove(result, i);
+	    throw;
     }
     return result;
 }
