5a0507290aa4859439e0b50f1e8b1f7364407a12
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-10-20 12:39:10 +0300

    declorate some functions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cstddef>
 
-
+int ** create(size_t rows, size_t cols);
+void remove (int ** m, size_t rows, size_t cols);
 
 int main () {
     size_t rows = 0, cols = 0;
@@ -9,5 +10,9 @@ int main () {
     if (!std::cin) {
 	return 1;
     }
-    std::cout << rows << " " << cols << "\n";
+//    std::cout << rows << " " << cols << "\n";
+    	
+    int ** m = create(rows, cols);
+
+    remove(m, rows, cols);
 }
