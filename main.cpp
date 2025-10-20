68021cd236704880b62cbd2f95bec85f71b8b709
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-10-20 21:49:23 +0300

    unuse convert, return to lesson version with function convert added

--- main.cpp
+++ main.cpp
@@ -8,36 +8,24 @@ void output(const int * const * m, size_t rows, size_t cols);
 int ** convert(const int * t, size_t n, size_t * lns, size_t rows);
 
 int main () {
-    // size_t rows = 0, cols = 0;
-    // std::cin >> rows >> cols;
-    // if (!std::cin) {
-	   //  return 1;
-    // }
-    //
-    // int ** m = create(rows, cols);
-    //
-    // input(m, rows, cols);
-    // if (!std::cin) {
-	   //  remove(m, rows);
-	   //  return 1;
-    // }
-    //
-    // output(m, rows, cols);
-    // std::cout << "\n";
-    //
-    // remove(m, rows);
-    size_t rows = 4;
-    size_t lns[4]{4, 2, 5, 1};
-    size_t n = 12;
-    int t[12]{5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
-    int ** res = convert(t, n, lns, rows);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < lns[i]; j++) {
-            std::cout << res[i][j] << " ";
-        }
-        std::cout << "\n";
+    size_t rows = 0, cols = 0;
+    std::cin >> rows >> cols;
+    if (!std::cin) {
+	    return 1;
     }
-    remove(res, rows);
+
+    int ** m = create(rows, cols);
+
+    input(m, rows, cols);
+    if (!std::cin) {
+	    remove(m, rows);
+	    return 1;
+    }
+
+    output(m, rows, cols);
+    std::cout << "\n";
+
+    remove(m, rows);
 }
 
 
