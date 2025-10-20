1c52d719e54f8db3645c129af630cbd50fd2a844
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-10-20 13:06:39 +0300

    remove

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cstddef>
 
 int ** create(size_t rows, size_t cols);
-void remove(int ** m, size_t rows, size_t cols);
+void remove(int ** m, size_t rows);
 void input(int ** m, size_t rows, size_t cols);
 void output(const int * const * m, size_t rows, size_t cols);  
 
@@ -17,31 +17,44 @@ int main () {
 
     input(m, rows, cols);
     if (!std::cin) {
-	remove(m, rows, cols);
+	remove(m, rows);
 	return 1;
     }
 
     output(m, rows, cols);
     std::cout << "\n";
 
-    remove(m, rows, cols);
+    remove(m, rows);
 }
 
+
+
 void input(int ** m, size_t rows, size_t cols) {
-    for (size_t i = 0ull; i < rows; i++) {
-	for (size_t j = 0ull; j < rows; j++) {
+    for (size_t i = 0ull; i < rows; ++i) {
+	for (size_t j = 0ull; j < rows; ++j) {
 	    std::cin >> m[i][j];
         }
     }
 }
 
 void output(const int * const * m, size_t rows, size_t cols) {
-    for (size_t i = 0ull; i < rows; i++) {
+    for (size_t i = 0ull; i < rows; ++i) {
 	std::cout << m[i][j];
-        for (size_t j = 1ull; j < rows; j++) {
+        for (size_t j = 1ull; j < rows; ++j) {
             std::cout << " " << m[i][j];
         }
 	std::cout << "\n";
     }
+}
+
+int ** create(size_t rows, size_t cols) {
+    
+}
+
+void remove(int ** m, size_t rows) {
+    for (size_t i = 0ull; i < rows; ++i) {
+        delete[] m[i];
+    }
+    delete[] m;
 }
 
