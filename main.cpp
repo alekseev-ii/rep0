50b2219589804632a74e67426abc773aee5dae51
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-10-20 12:25:13 +0300

    input rows and cols

--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
-int main () {}
+#include <iostream>
+#include <cstddef>
+
+
+
+int main () {
+    size_t rows = 0, cols = 0;
+    std::cin >> rows >> cols;
+    if (!std::cin) {
+	return 1;
+    }
+    std::cout << rows << " " << cols << "\n";
+}
